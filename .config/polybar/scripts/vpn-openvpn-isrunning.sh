bf62f00ccb806f28dc795d35fe2ed863b292d65a
Alexei Mikhailov <kblcuk@gmail.com>
2020-12-29 12:52:19 +0200

    Replace OpenVPN with Wireguard

--- .config/polybar/scripts/vpn-openvpn-isrunning.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-connection=$(pgrep -a openvpn | head -n 2 | rg --only-matching --replace '$1' 'remote ([a-zA-Z\.]+)')
-
-if [ -n "$connection" ]; then
-    echo "VPN: $connection"
-else
-    echo ""
-fi
