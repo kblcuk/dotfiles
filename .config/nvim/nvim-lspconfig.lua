f947bd1514e105d41c7559cbacdfe4312fc5f89c
Alexei Mikhailov <kblcuk@pm.me>
2021-09-18 19:24:04 +0300

    Sync formatting on save

--- .config/nvim/nvim-lspconfig.lua
+++ .config/nvim/nvim-lspconfig.lua
@@ -111,7 +111,7 @@ local efm_on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
         vim.api.nvim_command [[augroup Format]]
         vim.api.nvim_command [[autocmd! * <buffer>]]
-        vim.api.nvim_command [[autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()]]
+        vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
         vim.api.nvim_command [[augroup END]]
     end
 end
