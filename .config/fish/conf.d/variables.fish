2e319eae6ff94297e4e060797af606ccec422d2f
Alexei Mikhailov <kblcuk@pm.me>
2022-02-13 13:50:17 +0200

    Configure fish cursors

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -8,3 +8,9 @@ set -gx GPG_TTY (tty)
 
 fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/libpq/bin /opt/homebrew/opt/openjdk/bin
 fish_add_path /Users/alex/.cargo/bin
+
+set -gx fish_cursor_default block
+set -gx fish_cursor_insert line
+set -gx fish_cursor_replace underscore
+set -gx fish_cursor_replace_one underscore
+set -gx fish_cursor_visual block
