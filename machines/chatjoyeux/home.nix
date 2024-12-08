0171e6abf35774dbe14e99410930354f4e6f2cb5
Alexei Mikhailov <kblcuk@pm.me>
2024-12-08 13:22:30 +0200

    Remove unneeded path fix for fish
    ...which has been properly fixed as of 24.11.

--- machines/chatjoyeux/home.nix
+++ machines/chatjoyeux/home.nix
@@ -14,7 +14,7 @@ let
   };
 in
 {
-  # Auto-link .app to correct location, 
+  # Auto-link .app to correct location,
   # so spotlight finds them
   home-manager.sharedModules = [
     mac-app-util.homeManagerModules.default
@@ -123,34 +123,6 @@ in
         interactiveShellInit = ''
           fish_vi_key_bindings
         '';
-        # FIXME: This is needed to address bug where the $PATH is re-ordered by
-        # the `path_helper` tool, prioritising Apple’s tools over the ones we’ve
-        # installed with nix.
-        #
-        # This gist explains the issue in more detail: https://gist.github.com/Linerre/f11ad4a6a934dcf01ee8415c9457e7b2
-        # There is also an issue open for nix-darwin: https://github.com/LnL7/nix-darwin/issues/122
-        loginShellInit =
-          let
-            # We should probably use `config.environment.profiles`, as described in
-            # https://github.com/LnL7/nix-darwin/issues/122#issuecomment-1659465635
-            # but this takes into account the new XDG paths used when the nix
-            # configuration has `use-xdg-base-directories` enabled. See:
-            # https://github.com/LnL7/nix-darwin/issues/947 for more information.
-            profiles = [
-              "/etc/profiles/per-user/$USER" # Home manager packages
-              "$HOME/.nix-profile"
-              "(set -q XDG_STATE_HOME; and echo $XDG_STATE_HOME; or echo $HOME/.local/state)/nix/profile"
-              "/run/current-system/sw"
-              "/nix/var/nix/profiles/default"
-            ];
-
-            makeBinSearchPath = pkgs.lib.concatMapStringsSep " " (path: "${path}/bin");
-          in
-          ''
-            # Fix path that was re-ordered by Apple's path_helper
-            fish_add_path --move --prepend --path ${makeBinSearchPath profiles}
-            set fish_user_paths $fish_user_paths
-          '';
       };
       programs.starship.enable = true;
 
