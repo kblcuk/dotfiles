298602856fdbc93d4704d2a5db1a9c2537d61617
Alexei Mikhailov <alexei.mikhailov@socialfirstgames.com>
2025-01-02 11:54:05 +0200

    Enable go extra in lazyvim

--- dotfiles/lazyvim/lua/config/lazy.lua
+++ dotfiles/lazyvim/lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.vue" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.lang.nix" },
+		{ import = "lazyvim.plugins.extras.lang.go" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.editor.fzf" },
