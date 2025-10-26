6fdfc8d2e2f15e79dbdb42a488ab157bf6001a07
Alexei Mikhailov <alexei.mikhailov@socialfirstgames.com>
2025-10-26 13:00:49 +0200

    Use moon variant of rose-pine
    Default one is too dark.

--- dotfiles/lazyvim/lua/plugins/colorscheme.lua
+++ dotfiles/lazyvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
-  { "rose-pine/neovim", name = "rose-pine", lazy = false },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
-    },
-  },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		opts = {
+			dark_variant = "moon",
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "rose-pine",
+		},
+	},
 }
