8dce28c2a8e483bece0556554f80904933017919
Alexei Mikhailov <alexei.mikhailov@socialfirstgames.com>
2025-10-26 13:01:10 +0200

    Disable lazydev if luarc.json exists
    Otherwise it starts to scan the whole repository, which in case of just
    one lua file might be a bit pointless.

--- dotfiles/lazyvim/lua/plugins/lazydev.lua
+++ dotfiles/lazyvim/lua/plugins/lazydev.lua
@@ -8,6 +8,11 @@ return {
 			-- (where "test files" means it contains "describe" or "assert")
 			{ path = "${3rd}/busted/library", words = { "describe" } },
 			{ path = "${3rd}/luassert/library", words = { "assert" } },
+			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 		},
+		-- disable when a .luarc.json file is found
+		enabled = function(root_dir)
+			return not vim.uv.fs_stat(root_dir .. "/.luarc.json")
+		end,
 	},
 }
