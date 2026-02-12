c3ab6eab35ed89a57fb66baa7cd5359bd9001869
Alexei Mikhailov <kblcuk@pm.me>
2026-02-12 21:07:05 +0200

    Update home machine git config to work with 25.11

--- machines/joiedevirve/home.nix
+++ machines/joiedevirve/home.nix
@@ -1,7 +1,18 @@
 {
+  pkgs,
   nixpkgs,
+  inputs,
   ...
 }:
+
+let
+  delta_themes = pkgs.fetchFromGitHub {
+    owner = "dandavison";
+    repo = "delta";
+    rev = "acd758f7a08df6c2ac5542a2c5a4034c664a9ed8";
+    hash = "sha256-L9m5/o1I6Z5U8YdqaXsFVT3X+xvWafiz79IEAnUSLrk=";
+  };
+in
 {
   imports = [
     ../../common/base-home.nix
@@ -13,25 +24,42 @@
       # Add machine-specific packages to base packages
       home.packages = import ./packages.nix { inherit pkgs nixpkgs; };
 
-      programs.git = {
-        enable = true;
-        lfs.enable = true;
-        ignores = import ../chatjoyeux/gitignore_global.nix;
-        userName = "Alexei Mikhailov";
-        userEmail = "kblcuk@pm.me";
-        signing = {
-          key = "A983E913138CD12E";
-          signByDefault = true;
+      programs = {
+        git = {
+          enable = true;
+          lfs.enable = true;
+          ignores = import ../chatjoyeux/gitignore_global.nix;
+          settings = {
+            user = {
+              name = "Alexei Mikhailov";
+              email = "kblcuk@pm.me";
+            };
+            merge.conflictstyle = "diff3";
+            pull.rebase = true;
+            diff.colorMoved = "default";
+            core.autocrlf = false;
+            include = {
+              path = "${delta_themes}/themes.gitconfig";
+            };
+          };
+          signing = {
+            key = "A983E913138CD12E";
+            signByDefault = true;
+          };
         };
+
         delta = {
           enable = true;
-          options.navigate = true;
+          enableGitIntegration = true;
+          options = {
+            navigate = true;
+            features = "mellow-barbet";
+          };
         };
-        extraConfig = {
-          merge.conflictstyle = "diff3";
-          pull.rebase = true;
-          diff.colorMoved = "default";
-          core.autocrlf = false;
+
+        mise = {
+          enable = true;
+          package = inputs.nixpkgs.legacyPackages.${pkgs.system}.mise;
         };
       };
     };
